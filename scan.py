a48dcca640456c47c58880a1c12011bc0795254b
Colin Cox <cox@stsci.edu>
2015-09-25 13:58:00 -0400

    Added a test

--- scan.py
+++ scan.py
@@ -27,4 +27,5 @@ assert scan([1000000]) == 1000000
 assert scan([1, 2, 3, 4, 1]) == 0
 assert scan([1, 2, 3, 4, 1, 2]) == 2
 assert scan([1, 3, 5, 42, 1])
+assert scan([1000, 2000, 3000])
 #assert scan([1, 2, 3, 3, 4, 1, 2, 3]) == 5
